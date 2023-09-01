5ab43950cd509b756fec9bcbb133115e9243f897
abhishek <abhishek@maitretech.com>
2023-09-01 20:05:59 +0530

    image path change commit

--- src/components/Gener.js
+++ src/components/Gener.js
@@ -34,38 +34,36 @@ export default function Gener() {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
   return (
-    <div className="">
+    <div className="w-full">
       <h1 className="text-3xl m-5">Gener 1</h1>
 
-      <div className="flex flex-row ">
-        
-      <Carousel responsive={responsive}>
-        {movieList.movies.map((movie, index) => (
-            <div className="w-full">
-                
-            <img src={movie.poster} style={{margin:'20px', width:'500px',height:'200px'}} />
+      <div className="flex flex-row">
+        {/* <Carousel responsive={responsive}> */}
+          {movieList.movies.map((movie, index) => (
             
-            </div>
-        ))}
-        </Carousel>
-        {/*  */}
+              <img
+                src={movie.backdrop}
+                style={{ margin: '20px', width: '300px' }}
+              />
+          ))}
+        {/* </Carousel> */}
       </div>
     </div>
   );
